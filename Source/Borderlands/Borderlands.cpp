99b0027ae7e5d546c4cea8a36e5dcafb70aebd1b
yongaro <resodafk@gmail.com>
2016-03-01 16:09:31 +0100

    Reset

---
+++ Source/Borderlands/Borderlands.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#include "Borderlands.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Borderlands, "Borderlands" );
+ 
---
+++ Source/Borderlands/Borderlands.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __BORDERLANDS_H__
+#define __BORDERLANDS_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
