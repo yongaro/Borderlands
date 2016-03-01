fb03aea6c3b8f9eddf3d201ed63a46309c075b9e
Flow.L <elbarto347@gmail.com>
2016-03-01 16:17:42 +0100

    Absorbers + damageHandler
    Gestion des dommages

---
+++ Source/Borderlands/DamageHandler.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Absorber.h"
+#include "DamageHandler.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BORDERLANDS_API UDamageHandler : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDamageHandler();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	virtual BOOLEAN Damage(int damageAmount, FString type);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Absorbers")
+		TArray<UAbsorber*> absorbers;
+	
+};
